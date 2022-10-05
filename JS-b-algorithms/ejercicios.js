352e09216e37a8eff7f68e5042365503d2a8c2a8
“Cesc-greoles” <fgreoles@gmail.com>
2022-10-05 18:51:08 +0200

    prueba

---
+++ JS-b-algorithms/ejercicios.js
@@ -0,0 +1,8 @@
+const nombres = ['juan', 'alberto', 'laura', 'berta']
+const numeros = [25, 33, 44, 38]
+
+
+console.log(nombres.concat(numeros));
+console.log(numeros.sort);function sort (a, b) {
+    return a - b ;
+}
---
+++ JS-b-algorithms/prueba.js
@@ -0,0 +1,26 @@
+for ( let i = 30; i >= -10; i = i-2){
+    console.log(i);
+}
+
+for ( let z = 100; z <= 10; z = i/2){
+    console.log(z);
+}
+
+
+let pyramid="";
+
+
+
+for (let m=0; m < 10; m++){
+
+    for (let n= 0; n < m; n++){
+
+        pyramid += "*";
+
+    }
+
+    pyramid += "\n";
+
+}
+
+console.log(pyramid);
