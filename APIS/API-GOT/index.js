2f5d21838bbfe4b8712495da989996c2e63e2208
“Cesc-greoles” <fgreoles@gmail.com>
2022-10-15 12:24:05 +0200

    actualizacion GOT

--- APIS/API-GOT/index.js
+++ APIS/API-GOT/index.js
@@ -1,38 +1,72 @@
+h1$$ = document.createElement("h1");
+divSearch = document.createElement("div");
+const got$$ = document.createElement("div");
 
-const got$$ = document.createElement("div"); 
-got$$.className = "general";
-document.body.appendChild(got$$); 
+h1$$.textContent = "GAME OF THRONES";
+divSearch.className = "search";
+got$$.className = "got";
 
+document.body.appendChild(h1$$);
+document.body.appendChild(divSearch);
+document.body.appendChild(got$$);
 
-function pintar (characters) {
-    got$$.innerHTML = ""; 
+function onSearch(event, characters) {
+    event.preventDefault();
+    const value = document.getElementById("searchTerm").value;
+    console.log(value);
+    for (const character of characters) {
+        if (
+            character.fullName.toLowerCase().includes(value.toLowerCase()) ||
+            character.title.toLowerCase().includes(value.toLowerCase())
+        ) {
+            document.getElementById(character.id).classList.remove("hidden");
+            console.log(character.fullName, character.title);
+        } else {
+            document.getElementById(character.id).classList.add("hidden");
+        }
+    }
+}
+
+function pintar(characters) {
+    got$$.innerHTML = "";
+
+    const form$$ = document.createElement("form");
+    const input$$ = document.createElement("input");
+    const button$$ = document.createElement("button");
+    button$$.textContent = "SEARCH";
+    input$$.id = "searchTerm";
+
+    form$$.onsubmit = (event) => onSearch(event, characters);
+    form$$.appendChild(input$$);
+    form$$.appendChild(button$$);
+    divSearch.appendChild(form$$);
 
     for (const character of characters) {
         const carta$$ = document.createElement("div");
-        const titulo$$ = document.createElement("h3"); 
-        const img$$ = document.createElement("img"); 
+        const titulo$$ = document.createElement("h3");
+        const img$$ = document.createElement("img");
+        const p$$ = document.createElement("p");
 
-        carta$$.className = "cards"
-        titulo$$.className = "titulo"
-        img$$.className = "img"
+        carta$$.id = character.id;
 
-        titulo$$.textContent = character.title; 
-        img$$.src = character.image;
+        carta$$.className = "cards";
+        titulo$$.className = "titulo";
+        img$$.className = "img";
+        p$$.className = "full-name";
 
-        carta$$.appendChild(titulo$$);
+        titulo$$.textContent = character.title;
+        img$$.src = character.imageUrl;
+        p$$.textContent = character.fullName;
+
         carta$$.appendChild(img$$);
-        got$$.appendChild(carta$$); 
+        carta$$.appendChild(titulo$$);
+        carta$$.appendChild(p$$);
+        got$$.appendChild(carta$$);
     }
-    
-
 }
 
-
-
-
-
-
-
 fetch("https://thronesapi.com/api/v2/Characters")
-  .then((characters) => characters.json())
-  .then((characters) => { pintar(characters)});
+    .then((characters) => characters.json())
+    .then((characters) => {
+        pintar(characters);
+    });
---
+++ APIS/API-POKEMON/index.js
@@ -0,0 +1,6 @@
+// Requisitos 
+
+// 1. Obtener lista de pokedex y guardar la variable 
+// 2. Obtener el listado de todos los pokemons 
+// 3. Obtener todos los pokemons individuales uno por uno 
+// 4. Para Obtenertodos los pokemons, me dice el ejercicio que debo de iterar
