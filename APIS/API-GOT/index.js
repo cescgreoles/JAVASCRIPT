fe0a3feafe10397ce0741b553b73ccfab55d2793
“Cesc-greoles” <fgreoles@gmail.com>
2022-10-17 10:23:50 +0200

    API GOT: DivStart and images

--- APIS/API-GOT/index.js
+++ APIS/API-GOT/index.js
@@ -1,18 +1,29 @@
-fetch("https://thronesapi.com/api/v2/Characters")
-    .then((characters) => characters.json())
-    .then((characters) => {
-        pintar(characters);
-    });
-
 const divStart = document.createElement("div");
 const divSearch = document.createElement("div");
 const got$$ = document.createElement("div");
+const imgHeader = document.createElement("img");
+const imgHeader1 = document.createElement("img");
+const imgHeader2 = document.createElement("img");
 
 divStart.className = "first-container";
 divSearch.className = "search";
 got$$.className = "got";
+imgHeader.src =
+    "https://freepngimg.com/save/90816-head-silhouette-house-stark-daenerys-arya-targaryen/924x784";
+imgHeader1.src =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Game_of_Thrones_2011_logo.svg/1200px-Game_of_Thrones_2011_logo.svg.png";
+imgHeader2.src =
+    "https://popcornerreviews.com/wp-content/uploads/2019/05/Lannister-Logo.png";
+
+imgHeader.className = "imgHeader";
+imgHeader1.className = "imgHeader1";
+imgHeader2.className = "imgHeader2";
 
 document.body.appendChild(divStart);
+
+divStart.appendChild(imgHeader);
+divStart.appendChild(imgHeader1);
+divStart.appendChild(imgHeader2);
 document.body.appendChild(divSearch);
 document.body.appendChild(got$$);
 
@@ -37,8 +48,6 @@ function pintar(characters) {
     got$$.innerHTML = "";
 
     const img1$ = document.createElement("img");
-    img1$.src =
-        "https://1000logos.net/wp-content/uploads/2020/09/Game-of-Thrones-logo.png";
 
     const form$$ = document.createElement("form");
     form$$.className = "myform";
@@ -48,8 +57,7 @@ function pintar(characters) {
     input$$.id = "searchTerm";
 
     form$$.onsubmit = (event) => onSearch(event, characters);
-
-    img1$.appendChild(divStart);
+    form$$.autocomplete = "off";
     form$$.appendChild(input$$);
     form$$.appendChild(button$$);
     divSearch.appendChild(form$$);
@@ -77,3 +85,9 @@ function pintar(characters) {
         got$$.appendChild(carta$$);
     }
 }
+
+fetch("https://thronesapi.com/api/v2/Characters")
+    .then((characters) => characters.json())
+    .then((characters) => {
+        pintar(characters);
+    });
