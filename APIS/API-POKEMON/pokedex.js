374d15a980551a85b0136763641169b57a7f5fd0
“Cesc-greoles” <fgreoles@gmail.com>
2022-10-18 19:36:37 +0200

    Search and Colors Types POKEAPI

--- APIS/API-POKEMON/pokedex.js
+++ APIS/API-POKEMON/pokedex.js
@@ -1,3 +1,26 @@
+const typeColors = {
+    fire: "#ff7402",
+    grass: "#33a165",
+    steel: "#00858a",
+    water: "#0050ac",
+    psychic: "#c90086",
+    ground: "#c90086",
+    ice: "#70deff",
+    flying: "#5d4e75",
+    ghost: "#4d5b64",
+    normal: "#753845",
+    poison: "#7e0058",
+    rock: "#6e1a00",
+    fighting: "#634136",
+    dark: "#272625",
+    bug: "#6e1a00",
+    dragon: "#00c431",
+    electric: "#bba909",
+    fairy: "#d31c81",
+    unknow: "#757575",
+    shadow: "#29292c",
+};
+
 const divHeader = document.createElement("div");
 const divBienvenido = document.createElement("div");
 const divSearch = document.createElement("div");
@@ -46,7 +69,7 @@ function paint(poke) {
 
     input$$.id = "searchTerm";
 
-    form$$.onsubmit = (event) => onSearch(event, poke);
+    form$$.onsubmit = (event) => onSearch(event);
 
     imgButton.src =
         "https://i.pinimg.com/originals/f7/b4/1b/f7b41b0b170d8765d7f6684497c7763a.png";
@@ -68,97 +91,97 @@ function paint(poke) {
             .then((respons) => {
                 pokemonInfo(respons);
             });
-
-        console.log(pokemon);
-
-        function pokemonInfo(infoPokemon) {
-            const divCard = document.createElement("div");
-            divCard.className = "card";
-
-            const divTitleId = document.createElement("div");
-            divTitleId.className = "title-id";
-
-            const h1Poke = document.createElement("h1");
-            h1Poke.className = "card-title";
-            h1Poke.textContent = infoPokemon.name;
-
-            const idNumber = document.createElement("id");
-            idNumber.className = "id";
-            idNumber.textContent = infoPokemon.id;
-
-            const imgPoke = document.createElement("img");
-            imgPoke.className = "card-image";
-            imgPoke.src = infoPokemon.sprites.front_shiny;
-
-            const divImageSpan = document.createElement("div");
-            divImageSpan.className = "div-image-span";
-
-            const divSpans$$ = document.createElement("div");
-            divSpans$$.className = "div-spans";
-
-            const spanTypes = document.createElement("span");
-            spanTypes.textContent = "TYPES";
-            spanTypes.className = "span-types";
-
-            const spanAbilities = document.createElement("span");
-            spanAbilities.textContent = "ABILITIES";
-            spanAbilities.className = "span-abilities";
-
-            const span$$ = document.createElement("span");
-
-            for (const type of infoPokemon.types) {
-                span$$.textContent += type.type.name + " ";
-                console.log(type);
-            }
-
-            const span2$$ = document.createElement("span");
-
-            for (const ability of infoPokemon.abilities) {
-                span2$$.textContent += ability.ability.name + " ";
-                console.log(ability);
-            }
-
-            document.body.appendChild(divTitleId);
-            document.body.appendChild(h1Poke);
-            document.body.appendChild(idNumber);
-
-            document.body.appendChild(divImageSpan);
-            document.body.appendChild(imgPoke);
-            document.body.appendChild(divSpans$$);
-            document.body.appendChild(spanTypes);
-            document.body.appendChild(span$$);
-            document.body.appendChild(spanAbilities);
-            document.body.appendChild(span2$$);
-
-            divPokemons.appendChild(divCard);
-
-            divCard.appendChild(divTitleId);
-            divTitleId.appendChild(idNumber);
-            divTitleId.appendChild(h1Poke);
-            divCard.appendChild(divImageSpan);
-            divImageSpan.appendChild(imgPoke);
-            divImageSpan.appendChild(divSpans$$);
-
-            divSpans$$.appendChild(span$$);
-            divSpans$$.appendChild(spanAbilities);
-            divSpans$$.appendChild(span2$$);
-            divSpans$$.appendChild(spanTypes);
-            divSpans$$.appendChild(span$$);
-
-            console.log(infoPokemon);
-        }
     }
 }
 
-function onSearch(event, poke) {
+const allPokemonsInfo = [];
+
+function pokemonInfo(infoPokemon) {
+    allPokemonsInfo.push(infoPokemon);
+
+    const divCard = document.createElement("div");
+    divCard.className = "card";
+    divCard.id = infoPokemon.id;
+    divPokemons.appendChild(divCard);
+
+    const divTitleId = document.createElement("div");
+    divTitleId.className = "title-id";
+    divCard.appendChild(divTitleId);
+
+    const idNumber = document.createElement("id");
+    idNumber.className = "id";
+    idNumber.textContent = infoPokemon.id;
+    divTitleId.appendChild(idNumber);
+
+    const h1Poke = document.createElement("h1");
+    h1Poke.className = "card-title";
+    h1Poke.textContent = infoPokemon.name;
+    divTitleId.appendChild(h1Poke);
+
+    const divImageSpan = document.createElement("div");
+    divImageSpan.className = "div-image-span";
+    divCard.appendChild(divImageSpan);
+
+    const imgPoke = document.createElement("img");
+    imgPoke.className = "card-image";
+    imgPoke.src = infoPokemon.sprites.front_shiny;
+    divImageSpan.appendChild(imgPoke);
+
+    const divSpans$$ = document.createElement("div");
+    divSpans$$.className = "div-spans";
+    divImageSpan.appendChild(divSpans$$);
+
+    const spanAbilities = document.createElement("span");
+    spanAbilities.textContent = "ABILITIES";
+    spanAbilities.className = "span-abilities";
+    divSpans$$.appendChild(spanAbilities);
+
+    for (const ability of infoPokemon.abilities) {
+        const span2$$ = document.createElement("span");
+        span2$$.textContent += ability.ability.name + " ";
+        divSpans$$.appendChild(span2$$);
+    }
+
+    const spanTypes = document.createElement("span");
+    spanTypes.textContent = "TYPES";
+    spanTypes.className = "span-types";
+    divSpans$$.appendChild(spanTypes);
+
+    for (const type of infoPokemon.types) {
+        const span$$ = document.createElement("span");
+        span$$.textContent += type.type.name + " ";
+        span$$.style.color = typeColors[type.type.name];
+        divSpans$$.appendChild(span$$);
+    }
+
+    console.log(infoPokemon);
+}
+
+function onSearch(event) {
     event.preventDefault();
     const value = document.getElementById("searchTerm").value;
-    console.log(value);
-    for (const pokemon of poke.results) {
-        if (
-            pokemon.name.toLowerCase().includes(value.toLowerCase()) ||
-            pokemon.id.toLowerCase().includes(value.toLowerCase())
-        ) {
+
+    for (const pokemon of allPokemonsInfo) {
+        let showPokemon = false;
+        if (pokemon.name.toLowerCase().includes(value.toLowerCase()))
+            showPokemon = true;
+
+        if (pokemon.id == value) showPokemon = true;
+        console.log(pokemon);
+
+        for (const type of pokemon.types) {
+            if (type.type.name.toLowerCase().includes(value.toLowerCase()))
+                showPokemon = true;
+        }
+
+        for (const ability of pokemon.abilities) {
+            if (
+                ability.ability.name.toLowerCase().includes(value.toLowerCase())
+            )
+                showPokemon = true;
+        }
+
+        if (showPokemon) {
             document.getElementById(pokemon.id).classList.remove("hidden");
             console.log(pokemon.name, pokemon.id);
         } else {
